efbd33921901de116509f416ac4b3973ed6d26ad
David <20163661@aloe.ulima.edu.pe>
2020-09-14 08:03:29 -0500

    holamundo

--- src/main/java/com/app/edulearn/EdulearnApplication.java
+++ src/main/java/com/app/edulearn/EdulearnApplication.java
@@ -1,14 +1,20 @@
 package com.app.edulearn;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
 public class EdulearnApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EdulearnApplication.class, args);
-		//Aca programaremos la app
-	}
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(EdulearnApplication.class, args);
+    }
 }
