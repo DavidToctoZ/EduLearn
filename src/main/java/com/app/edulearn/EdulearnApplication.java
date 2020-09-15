c20c6f65673981f8c59d7cec70069e9424161f89
David <20163661@aloe.ulima.edu.pe>
2020-09-14 19:48:23 -0500

    cambio raiz

--- src/main/java/com/app/edulearn/EdulearnApplication.java
+++ src/main/java/com/app/edulearn/EdulearnApplication.java
@@ -1,20 +1,14 @@
 package com.app.edulearn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.*;
 
-@Controller
 @SpringBootApplication
+
 public class EdulearnApplication {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-      return "Hello World!";
-    }
-
     public static void main(String[] args) {
+        
         SpringApplication.run(EdulearnApplication.class, args);
     }
 }
+
