f98bc5ac32fb4071b93e9e77c19a676ebe850ee8
David <20163661@aloe.ulima.edu.pe>
2020-10-07 01:22:32 -0500

    prueba heroku png

--- src/main/java/com/app/edulearn/controller/WebController.java
+++ src/main/java/com/app/edulearn/controller/WebController.java
@@ -90,7 +90,7 @@ public class WebController {
           return "ingles5";
     } 
 
-    //Aritmetica ---------------------------------------------------------------
+    //Aritmetica ----------------------------------------------------------------
 
     @RequestMapping(value = "/aritmetica5", method = RequestMethod.GET)
       public String aritmetica5() {
